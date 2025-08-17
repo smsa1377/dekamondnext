e690e91c992d01a0be00949df9d2ee2a417feb7c
smsa1377 <smsa1377@gmail.com>
2025-08-17 14:13:33 +0330

    coding dashboard/page.tsx & dashboard.module.scss,

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,26 @@
+"use client";
+
+import {useEffect} from "react";
+import {useRouter} from "next/navigation";
+import {useAuth} from "@/hooks/useAuth";
+import styles from "./dashboard.module.scss";
+
 export default function DashboardPage() {
-    return(
-        <div>
-            Dashboard Page
+    const {user} = useAuth();
+    const router = useRouter();
+
+    useEffect(() => {
+        if (!user) router.push("/auth");
+    }, [user, router]);
+
+    if (!user) return null;
+
+    return (
+        <div className={styles.container}>
+            <h1>Welcome to the Dashboard</h1>
+            <p>
+                Hello, {user?.name?.first} {user?.name?.last} ({user?.email})
+            </p>
         </div>
-    )
+    );
 }
