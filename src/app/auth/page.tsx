f25b1425c955825967bd6589765f8114f8501af4
smsa1377 <smsa1377@gmail.com>
2025-08-17 15:55:04 +0330

    fixed error use client, npm i @hookform/resolvers, coding auth/page.tsx & auth.module.scss, before clean up

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,15 +1,47 @@
+"use client";
+import {useForm} from "react-hook-form";
+import {zodResolver} from "@hookform/resolvers/zod";
+import {loginSchema, LoginSchema} from "@/lib/validation";
+import {useAuth} from "@/hooks/useAuth";
 import Input from "@/components/ui/Input";
 import Button from "@/components/ui/Button";
 import styles from "./auth.module.scss";
 
 export default function AuthPage() {
-    return(
+    const {login} = useAuth();
+    const {
+        register,
+        handleSubmit,
+        formState: {errors, isSubmitting},
+    } = useForm<LoginSchema>({
+        resolver: zodResolver(loginSchema),
+    });
+
+    const onSubmit = async () => {
+        // const res = await fetch("https://randomuser.me/api/?results=1&nat=us");
+        const res = await fetch("@/data/api.json");
+        const data = await res.json();
+        const user = data.results[0];
+        login({
+            name: user.name,
+            email: user.email,
+        });
+    };
+
+    return (
         <div className={styles.container}>
-            <h2>Login Page</h2>
-            <>
-                <Input label="Email" type="email" placeholder="Email" required/>
-                <Button type="submit">Login</Button>
-            </>
+            <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+                <h2>Login Page</h2>
+                <Input
+                    label="Phone Number"
+                    type="text"
+                    {...register("phone")}
+                    error={errors.phone?.message}
+                />
+                <Button type="submit" disabled={isSubmitting}>
+                    {isSubmitting ? "Logging in..." : "Login"}
+                </Button>
+            </form>
         </div>
     )
 }
