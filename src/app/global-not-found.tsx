d9e5a15021db5e8676efb08b99d3a2f07fe65bfc
smsa1377 <smsa1377@gmail.com>
2025-10-18 00:17:10 +0330

    add metadata title and description for seo

--- src/app/global-not-found.tsx
+++ src/app/global-not-found.tsx
@@ -1,9 +1,20 @@
-"use client";
 import Link from "next/link";
 import styles from "./global-not-found.module.scss";
 
+import {Roboto} from 'next/font/google'
+import type {Metadata} from 'next'
+
+const roboto = Roboto({subsets: ['latin']})
+
+export const metadata: Metadata = {
+    title: '404 - Page Not Found',
+    description: 'The page you are looking for does not exist.',
+}
+
 export default function globalNotFound() {
     return (
+        <html lang="en" className={roboto.className}>
+        <body>
         <div className={styles.container}>
             <h1>I looked, it wasn't there. nope, nada, nothing.! 😕</h1>
             <p>Check the address or return to the login page.</p>
@@ -11,5 +22,7 @@ export default function globalNotFound() {
                 <button>Return to login</button>
             </Link>
         </div>
+        </body>
+        </html>
     );
 }
